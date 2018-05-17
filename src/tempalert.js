75bcd63064c74e64498abbc52209ebff7f00b7f2
Daniel <gomezd813@gmail.com>
2018-05-17 08:41:32 +0000

    made pause and play button. display current play time

--- src/tempalert.js
+++ src/tempalert.js
@@ -1,13 +1,58 @@
 import React, { Component } from 'react';
+
+
 class TempAlert extends Component {
+    componentDidMount() {
+        this.wavesurfer = window.WaveSurfer.create({
+            container: '#waveform',
+            waveColor: 'violet',
+            progressColor: 'purple'
+        });
+
+        this.wavesurfer.load('assets/atlas-shells.mp3');
+        this.wavesurfer.on('ready', () => {
+        this.wavesurfer.play();
+        setInterval(()=>{
+            console.log ("ayo wassup")
+            this.setState({
+                currentTime: this.wavesurfer.getCurrentTime()
+            })
+        }, 1000)
+        });
+    }
+
+    pause() {
+        this.wavesurfer.pause();
+    }
     
-    render () {
+    play() {
+        this.wavesurfer.play();
+    }
+    
+    getCurrentTime() {
+   if (this.state) {
+       return this.state.currentTime
+   }
+   else {
+       return ""
+   }
+    }
+
+    render() {
         return (
-        <div class="tempalertstyle">
-            <h1>no lolool </h1>
-            <h1>no this is patrick </h1>
-        </div>    
-            )
+        <div>
+            <div id="waveform"></div>
+            <button class="pause-button" onClick={()=>{this.pause()}}>
+                Pause
+            </button>
+            <button class="play-button" onClick={()=>{this.play()}}>
+                Play
+            </button>
+            <div class="get-time">
+                {this.getCurrentTime()}
+            </div>
+      </div>
+        )
     }
 }
 export default TempAlert;
